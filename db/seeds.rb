9ca002e83b768b173ef6ef4d9ef04aaaf8da0f39
Rodrigo Sánchez Isunza <rodrigoisunza@gmail.com>
2024-10-01 19:03:42 -0600

    Change logic of the requirements

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,31 +2,43 @@
 
 require 'faker'
 
-# Borra todos los datos existentes 
+# Borra todos los datos existentes (opcional)
 Doctor.destroy_all
 Patient.destroy_all
 Appointment.destroy_all
 
-# Crea doctores
-10.times do
-  Doctor.create!(
-    name: Faker::Name.name
-  )
-end
+# Crea doctores y pacientes
+Doctor.transaction do
+  10.times do
+    doctor = Doctor.create!(
+      name: Faker::Name.name
+      #email: Faker::Internet.email
+    )
 
-# Crea pacientes
-10.times do
-  Patient.create!(
-    name: Faker::Name.name,
-    doctor: Doctor.order('RANDOM()').first # Asigna un doctor aleatorio
-  )
-end
+    # Crea 10 pacientes únicos por cada doctor
+    10.times do
+      patient = Patient.create!(
+        name: Faker::Name.unique.name, # Asegura nombres únicos
+        doctor: doctor
+      )
 
-# Crea citas
-10.times do
-  Appointment.create!(
-    doctor: Doctor.order('RANDOM()').first, # Asigna un doctor aleatorio
-    patient: Patient.order('RANDOM()').first, # Asigna un paciente aleatorio
-    start_time: Faker::Time.forward(days: 23, period: :morning) # Hora de inicio aleatoria
-  )
+      # Crea 5 citas en el pasado
+      5.times do
+        Appointment.create!(
+          doctor: doctor,
+          patient: patient,
+          start_time: Faker::Time.backward(days: 30, period: :morning)
+        )
+      end
+
+      # Crea 5 citas en el futuro
+      5.times do
+        Appointment.create!(
+          doctor: doctor,
+          patient: patient,
+          start_time: Faker::Time.forward(days: 30, period: :morning)
+        )
+      end
+    end
+  end
 end
